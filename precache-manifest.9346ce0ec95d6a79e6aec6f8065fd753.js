70c4f83e0afca17c1f348cb5695307f890be1feb
KarolWojtas <karol.wojtas.aim@gmail.com>
2019-01-06 12:21:44 +0100

    Updates

---
+++ precache-manifest.9346ce0ec95d6a79e6aec6f8065fd753.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a66cce90fadd2f2f8a66",
+    "url": "/kws/static/js/runtime~main.a66cce90.js"
+  },
+  {
+    "revision": "8c0235492bbf9c3f6d84",
+    "url": "/kws/static/js/main.8c023549.chunk.js"
+  },
+  {
+    "revision": "8e2c0cd2f94e3a89002d",
+    "url": "/kws/static/js/1.8e2c0cd2.chunk.js"
+  },
+  {
+    "revision": "8c0235492bbf9c3f6d84",
+    "url": "/kws/static/css/main.9325c340.chunk.css"
+  },
+  {
+    "revision": "8e2c0cd2f94e3a89002d",
+    "url": "/kws/static/css/1.7b961f05.chunk.css"
+  },
+  {
+    "revision": "d4f3248f7b567245433717468fd2c97d",
+    "url": "/kws/index.html"
+  }
+];
