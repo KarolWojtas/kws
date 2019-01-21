7d83b9dbab1d94bfd07ce6638793379bfcc80e48
Karol <karol.wojtas.aim@gmail.com>
2019-01-21 20:26:27 +0000

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/kws/precache-manifest.33d311736aafcce131dcf238f4a5809f.js"
+  "/kws/precache-manifest.45824aa09f6309dae0222d9de231bc40.js"
 );
 
 workbox.clientsClaim();
